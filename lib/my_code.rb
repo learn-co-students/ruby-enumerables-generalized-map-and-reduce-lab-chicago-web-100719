86b518a0e98813404f959600eef80ed6c428b30d
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-09-19 14:56:24 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(array)
+  new_array = Array.new
+  i = 0
+  while i < array.length
+    new_array << yield(array[i])
+    i += 1
+  end
+  new_array
+end
+
+def reduce(array, sp = nil)
+  if sp
+    total = sp
+    i = 0
+  else
+    total = array[0]
+    i = 1
+  end
+  while i < array.length
+    total = yield(total, array[i])
+    i += 1
+  end
+  total
+end
