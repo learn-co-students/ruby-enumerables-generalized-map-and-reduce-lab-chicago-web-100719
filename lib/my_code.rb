f2bee13681cb4cbec5da16063e709808f84af615
Benjamin Davis <bennyadave@gmail.com>
2019-09-23 03:44:12 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  array.length.times do |index|
+    new_array[index] = yield(array[index])
+  end
+  return new_array
+end
+
+def reduce(array, tally = nil)
+  if tally
+    sum = tally
+    i = 0 
+  else
+    sum = array[0]
+    i = 1 
+  end 
+  while i < array.length
+    sum = yield(sum, array[i])
+    i += 1 
+  end
+  sum
+end
+  
+  
