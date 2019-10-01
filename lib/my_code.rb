feca2512e126357021bb62b47e620e463ea81412
Patrick Bosman <bsmn.ptrck@gmail.com>
2019-10-01 04:30:26 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,25 @@
-def map(element1,&block) 
-  element1.map(&block)
+def map(source_array)
+  new_array = []
+  i = 0
+  while source_array.length > i do
+    new_array.push(yield(source_array[i]))
+    i += 1
   end
-def reduce(source_array, starting_point = 0, &block)
-  source_array.reduce(&block)
+  new_array
+end
+
+def reduce(source_array, starting_point= nil)
+  if starting_point
+    answer = starting_point
+    i = 0
+  else
+    answer = source_array[0]
+    i = 1
+  end
+
+  while source_array.length > i
+    answer = yield(answer, source_array[i])
+    i += 1
+  end
+  answer
 end
